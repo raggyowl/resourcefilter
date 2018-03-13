618600e045110c09cea2ed4a358b47be7ac39611
Kuznetsov <walkohm@gmail.com>
2018-03-13 15:21:16 +0300

    Update FIlterPlugin.groovy

--- src/main/groovy/org/walkohm/resourcefilter/FIlterPlugin.groovy
+++ src/main/groovy/org/walkohm/resourcefilter/FIlterPlugin.groovy
@@ -13,34 +13,33 @@ class FilterPlugin implements Plugin<Project> {
     def pathToResources = 'src/main/resources/'
     @Override
     void apply(Project project) {
-        def extensions = project.extensions.create(
-                "resurcefilter",
-                ResourceFilterExtension.class)
+        def extension = project.extensions.create('resourceFilter', ResourceFilterExtension)
         def sourceSets = (SourceSetContainer) project.getProperties().get('sourceSets')
         def srcDirs = sourceSets.getByName('main').getResources().getSrcDirs()
-        extensions.getProfiles().each {profile->
+        project.afterEvaluate {
+            extensions.getProfiles().each {profile->
 
-            def profileDir = new File(pathToResources+ profile + '/')
-            if(!profileDir.exists())
+             def profileDir = new File(pathToResources+ profile + '/')
+                if(!profileDir.exists())
                 profileDir.mkdir()
-            project.tasks.create( profile +  "Build") {
-                doLast {
-                    println profile.toUpperCase() +  " BUILD"
-                    srcDirs = [pathToResources+ profile + '/']
+                project.tasks.create( profile +  "Build") {
+                    doLast {
+                        println profile.toUpperCase() +  " BUILD"
+                        srcDirs = [pathToResources+ profile + '/']
 
-                }
+                 }
 
-            }.finalizedBy(project.tasks.getByName('build'))
-            project.tasks.create(profile + "Run") {
-                doLast {
-                    println profile.toUpperCase() +  " RUN"
-                    srcDirs = [pathToResources+ profile + '/']
+                }.finalizedBy(project.tasks.getByName('build'))
+                project.tasks.create(profile + "Run") {
+                    doLast {
+                        println profile.toUpperCase() +  " RUN"
+                        srcDirs = [pathToResources+ profile + '/']
 
-                }
+                 }
 
-            }.finalizedBy(project.tasks.getByName('run'))
+                }.finalizedBy(project.tasks.getByName('run'))
 
+            }
         }
-
     }
 }
