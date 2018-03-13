79252dd5df0a1ae2a8d879784c2ed2cc1e357a88
Kuznetsov <walkohm@gmail.com>
2018-03-13 13:12:56 +0300

    Create FIlterPlugin.groovy

---
+++ src/main/groovy/org/walkohm/resourcefilter/FIlterPlugin.groovy
@@ -0,0 +1,46 @@
+package org.walkohm.resourcefilter
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.SourceSetContainer
+
+class ResourceFilterExtension {
+    String[] profiles = ["dev","release"]
+}
+
+
+class FilterPlugin implements Plugin<Project> {
+    def pathToResources = 'src/main/resources/'
+    @Override
+    void apply(Project project) {
+        def extensions = project.extensions.create(
+                "resurcefilter",
+                ResourceFilterExtension.class)
+        def sourceSets = (SourceSetContainer) project.getProperties().get('sourceSets')
+        def srcDirs = sourceSets.getByName('main').getResources().getSrcDirs()
+        extensions.getProfiles().each {profile->
+
+            def profileDir = new File(pathToResources+ profile + '/')
+            if(!profileDir.exists())
+                profileDir.mkdir()
+            project.tasks.create( profile +  "Build") {
+                doLast {
+                    println profile.toUpperCase() +  " BUILD"
+                    srcDirs = [pathToResources+ profile + '/']
+
+                }
+
+            }.finalizedBy(project.tasks.getByName('build'))
+            project.tasks.create(profile + "Run") {
+                doLast {
+                    println profile.toUpperCase() +  " RUN"
+                    srcDirs = [pathToResources+ profile + '/']
+
+                }
+
+            }.finalizedBy(project.tasks.getByName('run'))
+
+        }
+
+    }
+}
